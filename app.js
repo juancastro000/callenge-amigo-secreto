c35757d6ad63f51bfc214af332700faaa34d8e15
juan-castro <cdyutub@gmail.com>
2025-08-09 19:36:08 +0200

    feat: se agrego la funcion de sumar un nombre a la lista

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const nombres = [];
+
+const input = document.getElementById('amigo');
+const lista = document.getElementById('listaAmigos');
+const resultado = document.getElementById('resultado');
+
+function agregarNombre() {
+    const nombre = input.value.trim();
+    if (!nombre) {
+        resultado.textContent = "introduce un nombre";
+        return;
+    }
+
+    if (nombres.includes(nombre)) {
+        resultado.textContent = "ese nombre ya esta en la lista";
+        return;
+    }
+
+    nombres.push(nombre);
+    input.value ="";
+    resultado.textContent = '';
+}
